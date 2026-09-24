3f29c2b7fe9e133df08538535cad73bb507709f8
roushan404 <roushan5395r@gamil.com>
2026-09-25 01:06:23 +0530

    modifications

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
-const sidebar = () => {
+const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
